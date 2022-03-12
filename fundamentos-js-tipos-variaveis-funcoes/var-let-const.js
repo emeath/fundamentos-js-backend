a820807dd59998618127d1860d567f285ac342dc
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-12 13:15:14 -0300

    Adiciona tipos de variaveis - var, let e const

---
+++ fundamentos-js-tipos-variaveis-funcoes/var-let-const.js
@@ -0,0 +1,14 @@
+var souGlobal = 190
+let souLocaleRecente = 2022 //Escopo local. Melhor pratica utiliza-lo
+const souLocalConstanteRecente = 200 //Escolo local. Inalteravel. Melhor pratica utiliza-lo
+
+console.log(souGlobal)
+
+let base = 3;
+let altura = 4;
+area = (base * altura) / 2 //JS determinar que sera var
+
+console.log(typeof area)
+console.log(area)
+
+
