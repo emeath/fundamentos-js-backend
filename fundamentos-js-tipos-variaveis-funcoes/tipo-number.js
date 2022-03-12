fb1f39a102a457be10cb89a9befbaa1ba48fabdd
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-12 12:59:12 -0300

    Adiciona 2/3 tipos primitivos do js

---
+++ fundamentos-js-tipos-variaveis-funcoes/tipo-number.js
@@ -0,0 +1,8 @@
+//Tipo number eh um dos 3 tipos primitivos do JS
+const meuNumero = 42;
+
+console.log(meuNumero)
+
+const anoAtual = 2022
+const minhaIdade = 2022 - 1993
+console.log(`Minha idade em ${anoAtual} é ${minhaIdade}`)
---
+++ fundamentos-js-tipos-variaveis-funcoes/tipo-string.js
@@ -0,0 +1,10 @@
+//String é outro tipo primitivo de javascript (todos eles sao Number, String, Boolean)
+const texto = "Hello world!"
+const cifra = "0xFAFFA1332213AFFA << 0xFFFFFFFFFFFF - o que é isso? ???? :("
+
+console.log(texto)
+console.log(cifra)
+
+const citacao = 'Uma citação famosa "Ser ou não ser? Eis a questão" :)'
+
+console.log(citacao)
