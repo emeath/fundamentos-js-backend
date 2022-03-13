5732ed84e809cdb4862fd4bb72c8fbda5161bca1
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-13 19:54:56 -0300

    Adiciona template literals

---
+++ fundamentos-js-tipos-variaveis-funcoes/template-string.js
@@ -0,0 +1,14 @@
+const nome = 'Matheus'
+const idade = 28
+const suco = 'Graviola'
+const aprendendo = 'JavaScript'
+
+// Forma 1) utilzando concatenacao
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos. Gosto de suco sabor " + suco + " e no momento estou aprendendo a linguagem de programação " + aprendendo + " =)")
+
+// Forma 2) utilizando template string
+console.log(`Meu nome é ${nome} e tenho ${idade} anos. Gosto de suco sabor ${suco} e no momento estou aprendendo a linguagem de programação ${aprendendo} =)`)
+
+/**
+ * Forma 2 é a minha preferida.
+ */
