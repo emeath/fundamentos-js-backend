abc4f108cde0c92888ee414c79b0073118c3ebbc
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-13 19:17:09 -0300

    Adiciona um pouco mais sobre console.api

---
+++ fundamentos-js-tipos-variaveis-funcoes/console.js
@@ -0,0 +1,12 @@
+// log >> utilizado para info, registro
+
+const minhaVar = true;
+
+console.log(123);
+console.log(`minha var é ${minhaVar}`)
+console.log("Sou uma string")
+
+// error >> utilizado quando ocorrer algo na aplicação
+// Tratamento de error
+
+console.error("Ocorreu erro! Warning!")
