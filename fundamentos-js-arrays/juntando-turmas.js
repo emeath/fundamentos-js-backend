db18dff0bd80438c2ef427f7d71d79e2808c9c47
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-14 22:26:04 -0300

    Adiciona trabalho com metodo concat() de arrays

---
+++ fundamentos-js-arrays/juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaDePython = ["Malu", "Pudi", "Carvão"];
+const salaDeJavaScript = ["Kiko", "Katy", "Amarelo"];
+
+const salasUnificadas = salaDePython.concat(salaDeJavaScript)
+
+console.log(salasUnificadas);
