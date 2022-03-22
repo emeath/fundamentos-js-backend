1422e6c8e4a05ee9f8bf397bb6cbfa40ecdd64fd
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-22 19:53:57 -0300

    Adiciona forma alternativa utilizando arrow function externa como arg do .forEach()

--- fundamentos-js-objetos/03-acessando-chaves.js
+++ fundamentos-js-objetos/03-acessando-chaves.js
@@ -13,6 +13,22 @@ console.log(cliente[chave]) // Andre
 
 //Acessando todos os elementos de forma enxuta
 chaves.forEach(elemento => console.log(`Chave: ${elemento} - Valor: ${cliente[elemento]}`))
+/*
+Chave: nome - Valor: Andre
+Chave: idade - Valor: 36
+Chave: cpf - Valor: 1233321123
+Chave: email - Valor: ANDRE@EMAIL.COM
+*/
+
+// Forma alternativa
+const exibeElementosFormatados = elementoArray => console.log(`Chave: ${elementoArray} | Valor: ${cliente[elementoArray]}`)
+chaves.forEach(exibeElementosFormatados)
+/*
+Chave: nome | Valor: Andre
+Chave: idade | Valor: 36
+Chave: cpf | Valor: 1233321123
+Chave: email | Valor: ANDRE@EMAIL.COM
+*/
 
 // Tentar acessar objetos nao definidos retorna undefined e nao ocorre erro em JS...
 console.log(cliente.saldo) //undefined
