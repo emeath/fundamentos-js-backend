fe3234586f836e784f3f692328fc4033ac0e86a7
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-22 18:46:34 -0300

    Adiciona primeiro contato com objeto em js

---
+++ fundamentos-js-objetos/01-objetos.js
@@ -0,0 +1,18 @@
+const listaAlturas = [1.53, 1.81, 2.03, 1.73, 1.64]
+
+const clienteNutricao = ['nome', 'Matheus', 'idade', 50]
+
+clienteNutricao[0] // 'nome'
+clienteNutricao[1] // 'Matheus'
+clienteNutricao[2] // 'idade'
+clienteNutricao[3] // 50
+
+// anatomia de um objeto
+const objetoClienteNutricao = {
+    nome: 'Matheus',
+    idade: 50
+}
+
+// Mais clean, organizado e estrutuado na minha opiniao
+objetoClienteNutricao['nome'] // 'Matheus'
+objetoClienteNutricao['idade'] // 50
