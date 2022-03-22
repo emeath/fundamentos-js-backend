aaa24564bed1f92710bc955e57dc40407f603ab6
MartinsAtSolarvolt <matheus.martins@solarvoltenergia.com.br>
2022-03-22 19:02:46 -0300

    Adiciona conteudo no arquivo de acesso a informacoes do objeto

--- fundamentos-js-objetos/02-print-infos.js
+++ fundamentos-js-objetos/02-print-infos.js
@@ -1,7 +1,10 @@
 const cliente = {
     nome: 'Kiko',
     idade: 21,
-    cpf: 'cpfCanino000221',
+    cpf: '000221-cpfCanino',
     email: 'kikoOrei@dog.com'
 }
 
+console.log(cliente.nome)
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos de idade`)
+console.log(`Por questões de segurança, meus três primeiros dígitos do cpf é: ${cliente.cpf.substring(0, 3)}`)
